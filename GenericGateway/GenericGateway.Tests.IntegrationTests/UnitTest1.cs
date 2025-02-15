e95313e427d2f6fbdcc61c0a1b405d23ac96fda1
BakPeter <bak.pet@gmail.com>
2025-02-15 07:13:44 +0200

    Projects structure Init

---
+++ GenericGateway/GenericGateway.Tests.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace GenericGateway.Tests.IntegrationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
